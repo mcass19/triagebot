1573f85e5f45493914ffdfddaeee43caee2b7e40
Mauricio Cassola <mauricio@wyeworks.com>
2022-09-05 18:12:04 -0300

    More event helpers improvements

--- src/db/events.rs
+++ src/db/events.rs
@@ -15,23 +15,66 @@ pub struct Event {
     pub failed: Option<String>,
 }
 
-pub async fn insert_event(db: &DbClient) -> Result<()> {
-    unimplemented!();
+pub async fn insert_event(db: &DbClient, event: &Event) -> Result<()> {
+    tracing::trace!("insert_event(id={})", event.event_id);
+    
+    db.execute(
+        "INSERT INTO events (event_id, event_name, expected_event_time, event_metadata, executed_at, failed) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
+        &[&event.event_id, &event.event_name, &event.expected_event_time, &"", &event.executed_at, &event.failed],
+    )
+    .await
+    .context("inserting event")?;
+
+    Ok(())
 }
 
-pub async fn delete_event(db: &DbClient) -> Result<()> {
-    unimplemented!();
+pub async fn delete_event(db: &DbClient, event_id: &Uuid) -> Result<()> {
+    tracing::trace!("delete_event(id={})", event_id);
+    
+    db.execute(
+        "DELETE FROM events WHERE event_id = $1",
+        &[&event_id],
+    )
+    .await
+    .context("deleting event")?;
+
+    Ok(())
 }
 
-pub async fn update_event(db: &DbClient) -> Result<()> {
-    unimplemented!();
+pub async fn update_event_failed_message(db: &DbClient, event_id: &Uuid, message: &String) -> Result<()> {
+    tracing::trace!("update_event_failed_message(id={})", event_id);
+    
+    db.execute(
+        "UPDATE events SET failed = $2 WHERE event_id = $1",
+        &[&event_id, &message],
+    )
+    .await
+    .context("updating event failed message")?;
+
+    Ok(())
 }
 
+pub async fn update_event_executed_at(db: &DbClient, event_id: &Uuid) -> Result<()> {
+    tracing::trace!("update_event_executed_at(id={})", event_id);
+    
+    db.execute(
+        "UPDATE events SET executed_at = now() WHERE event_id = $1",
+        &[&event_id],
+    )
+    .await
+    .context("updating event executed at")?;
+
+    Ok(())
+}
+
+// Selects all events with:
+//  - event_time's in the past 
+//  - failed is null or executed_at is at least 60 minutes ago (intended to make repeat executions rare enough)
 pub async fn get_events_to_execute(db: &DbClient) -> Result<Vec<Event>>  {
     let events = db
         .query(
             "
-        SELECT * FROM events",
+        SELECT * FROM events WHERE expected_event_time <= now() AND (failed IS NULL OR executed_at <= now() - INTERVAL '60 minutes')",
             &[],
         )
         .await
