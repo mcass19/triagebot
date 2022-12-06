0baf9e64ca77121ff1ae6b487ea76cd13389924c
Mauricio Cassola <mauricio@wyeworks.com>
2022-12-06 11:05:51 -0300

    Schedule job for initial merge

--- src/handlers/jobs.rs
+++ src/handlers/jobs.rs
@@ -4,8 +4,17 @@
 
 // Further info could be find in src/jobs.rs
 
+use crate::github::*;
+use crate::handlers::decision::{DecisionProcessActionMetadata, DECISION_PROCESS_JOB_NAME};
+use parser::command::decision::Resolution::Merge;
+use reqwest::Client;
+use tracing as log;
+
 pub async fn handle_job(name: &String, metadata: &serde_json::Value) -> anyhow::Result<()> {
     match name {
+        matched_name if *matched_name == DECISION_PROCESS_JOB_NAME.to_string() => {
+            decision_process_handler(&metadata).await
+        }
         _ => default(&name, &metadata),
     }
 }
@@ -16,6 +25,31 @@ fn default(name: &String, metadata: &serde_json::Value) -> anyhow::Result<()> {
         name,
         metadata
     );
+
+    Ok(())
+}
+
+async fn decision_process_handler(metadata: &serde_json::Value) -> anyhow::Result<()> {
+    tracing::trace!(
+        "handle_job fell into decision process case: (metadata={:?})",
+        metadata
+    );
+
+    let metadata: DecisionProcessActionMetadata = serde_json::from_value(metadata.clone())?;
+
+    match metadata.status {
+        Merge => {
+            let gh_client = GithubClient::new_with_default_token(Client::new().clone());
+
+            let request = gh_client.get(&metadata.get_issue_url);
+
+            match gh_client.json::<Issue>(request).await {
+                Ok(issue) => issue.merge(&gh_client).await?,
+                Err(e) => log::error!("Failed to get issue {}, error: {}", metadata.get_issue_url, e),
+            }
+        }
+        _ => {}
+    }
 
     Ok(())
 }
